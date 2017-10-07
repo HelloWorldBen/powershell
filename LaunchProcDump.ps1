b435f59d928ce50c5ddb6a01fd2d8749cc279e1b
HelloWorldBen <32596965+HelloWorldBen@users.noreply.github.com>
2017-10-07 13:42:48 -0400

    Update LaunchProcDump.ps1

--- LaunchProcDump.ps1
+++ LaunchProcDump.ps1
@@ -1,4 +1,4 @@
-﻿
+
 <#
 
 ProcDump Script
@@ -6,22 +6,54 @@ ProcDump Script
 #>
 
 
+try
+{
+    
+
+
+
 #give the process name without exe 
 $processName = 'Notepad'
 
-#will get the process id 
-$procid=get-process $processName |select -expand id
+#will process list
+$processes = Get-Process $processName |select -expand id 
 
-#set the working directory location of the dump files
-$workingDirectory = 'D:\powershell\test\'
+ 
+#for each id loop through and get the dumps 
+foreach ($process in $processes){ 
+  
+$workingDirectory = 'D:\powershell\test'
 
 
 #give the location of procdump as it per remote machine
 $procdump =  'D:\powershell\procdump.exe'
 
 #give the parameters for procdump then concat with process id 
-$arguments = '-accepteula -ma -l  -n 5 ' +$procid
+$arguments = '-accepteula -ma -l  -n 5 ' +$process.ToString()
+
+ 
 
 #starts procdump with arguments place the files in working directory 
 Start-Process -FilePath $procdump -ArgumentList $arguments -WorkingDirectory $workingDirectory
+
+
+#launch a new instance of procdump after 20 seconds
+Start-Sleep -s 20
+
+}
+    
+
+
+    Start-Sleep -s 500
+    Write-Host 'Completed successfuly'
+
+}
+
+ Catch
+{
+    $ErrorMessage = $_.Exception.Message
+    Write-Host 'Error Message'
+}
+ 
+
 
