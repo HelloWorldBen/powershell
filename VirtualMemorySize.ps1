69293e6a39522b4f1947e2bf96003a321b92d9e3
HelloWorldBen <32596965+HelloWorldBen@users.noreply.github.com>
2017-12-03 08:41:47 -0500

    Add files via upload
    get virtual memory size

---
+++ VirtualMemorySize.ps1
@@ -0,0 +1,12 @@
+﻿$oProcesses= Get-WmiObject -Class Win32_Process
+$aObjects=@()
+ForEach($oProcesses in $oProcesses)
+{
+ $oMem = $oProcesses.GetAvailableVirtualSize()
+ $oObject=New-Object pscustomobject
+ Add-Member -InputObject $oObject -MemberType NoteProperty -Name 'Name'  -Value ($oProcesses.Name) -Force
+
+  Add-Member -InputObject $oObject -MemberType NoteProperty -Name 'AvailableVirtualSize' -Value $($oMem.AvailableVirtualSize) -Force
+  $aObjects+=@($oObject)
+}
+$aObjects | Sort-Object AvailableVirtualSize -Descending | ft -AutoSize
