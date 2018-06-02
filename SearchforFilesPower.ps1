fe9818a6bdda7c1cedd4bdfd151b358b2be17452
HelloWorldBen <32596965+HelloWorldBen@users.noreply.github.com>
2018-06-02 10:47:34 -0400

    Add files via upload

---
+++ SearchforFilesPower.ps1
@@ -0,0 +1,20 @@
+﻿param(
+    [string] $match = "Add",
+    [string]$dir = "D:\",
+    [string]$filter = "*.cs"
+)
+
+Write-Host "---------------------------------Start---------------------------------"
+
+$files = Get-ChildItem -Path $dir -Filter $filter -Recurse
+
+foreach ($file in $files) {
+    $content = $file | Get-Content
+
+    if ($content -match $match) {
+        Write-Host $file.FullName
+        $content | Select-String -Pattern $match | Select-Object -Property LineNumber, Line | Out-Host
+    }
+}
+
+Write-Host "---------------------------------End---------------------------------"
